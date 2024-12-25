6274d00a8bd42bc721cadde526efd9fcb513148b
Tero Keränen <tero-keranen@hotmail.com>
2024-12-25 20:00:10 +0200

    testing

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -31,7 +31,7 @@ router.get('/api/devices/:userId', async (req,res) => {
         if (!user) {
             return res.status(404).json({error: 'User not found'});
         }
-        console.log("User informations: ", user);
+        console.log("User informations: ", user.spaces);
         
     } catch (error) {
         
